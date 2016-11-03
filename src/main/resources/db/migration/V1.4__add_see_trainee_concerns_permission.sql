39e72ce0564f3b9eb33b9dda157c2f5ad29abdb1
naveen manga <naveen.manga@transformuk.com>
2016-11-03 10:04:58 +0000

    TISDEV-1162 Added permission for RVAdmin to view concerns

---
+++ src/main/resources/db/migration/V1.4__add_see_trainee_concerns_permission.sql
@@ -0,0 +1,3 @@
+
+insert IGNORE into permission (name) values ('concerns:see:trainee:concerns');
+insert IGNORE into role_permissions (role_name,permission_name) values ('RVAdmin','concerns:see:trainee:concerns');
