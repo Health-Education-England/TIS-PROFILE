678d8239c0bf083d0f71200fb70a326f71cfc609
naveen manga <naveen.manga@transformuk.com>
2016-11-10 17:03:32 +0000

    TISDEV-1217 Dropped all old tables scripts as we are now using UpperCamelCase tables.

--- src/main/resources/db/migration/V1.4__add_see_trainee_concerns_permission.sql
+++
@@ -1,3 +0,0 @@
-
-insert IGNORE into permission (name) values ('concerns:see:trainee:concerns');
-insert IGNORE into role_permissions (role_name,permission_name) values ('RVAdmin','concerns:see:trainee:concerns');
