39c0a0b8468249bf78a006dc4c29c319715b8d55
alexdobre <alx.dobre@gmail.com>
2016-12-28 11:56:49 +0000

    TISDEV-802 Assign Trainee to Admin - API

--- src/main/java/com/transformuk/hee/tis/auth/service/LoginService.java
+++ src/main/java/com/transformuk/hee/tis/auth/service/LoginService.java
@@ -3,6 +3,7 @@ package com.transformuk.hee.tis.auth.service;
 import com.transformuk.hee.tis.auth.model.User;
 import com.transformuk.hee.tis.auth.model.UserListResponse;
 import com.transformuk.hee.tis.auth.repository.UserRepository;
+import org.slf4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.List;
 
 import static com.transformuk.hee.tis.auth.filters.UserSpecification.isEqualTo;
 import static java.lang.String.format;
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.data.domain.Sort.Direction.ASC;
-import static org.springframework.transaction.annotation.Propagation.REQUIRED;
 import static org.springframework.util.StringUtils.isEmpty;
 
 /**
@@ -25,6 +28,7 @@ import static org.springframework.util.StringUtils.isEmpty;
 @Service
 @Transactional(readOnly = true)
 public class LoginService {
+	private static final Logger LOG = getLogger(LoginService.class);
 	
 	private final UserRepository userRepository;
 
@@ -56,13 +60,18 @@ public class LoginService {
 		int pageNumber = offset / limit;
 		Pageable page = new PageRequest(pageNumber, limit, new Sort(ASC, "firstName"));
 		Page<User> users;
-		if (!isEmpty(filter)) {
-			String[] nameValue = filter.split("=");
-			users = userRepository.findAll(isEqualTo(nameValue[0], nameValue[1]), page);
-		} else {
-			users = userRepository.findAll(page);
+		try {
+			if (!isEmpty(filter)) {
+				String[] nameValue = URLDecoder.decode(filter, "UTF-8").split("=");
+				users = userRepository.findAll(isEqualTo(nameValue[0], nameValue[1]), page);
+			} else {
+				users = userRepository.findAll(page);
+			}
+			return new UserListResponse(users.getTotalElements(), users.getContent());
+		} catch (UnsupportedEncodingException e) {
+			LOG.error("this should never happen", e);
 		}
-		return new UserListResponse(users.getTotalElements(), users.getContent());
+		return null;
 	}
 
 	/**
