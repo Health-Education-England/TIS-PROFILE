f535282030439d46109956c511aae5eb3236b034
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-04-21 13:16:13 +0100

    permissions for reference service

---
+++ profile-service/src/main/resources/db/migration/V1.3__TISDEV-1754_Reference_Permissions.sql
@@ -0,0 +1,15 @@
+-- new permissions for reference
+INSERT INTO `Permission` (`name`)
+VALUES
+('reference:add:modify:entities'), ('reference:delete:entities');
+
+-- new roles for reference
+INSERT INTO `Role` (`name`)
+VALUES
+('ReferenceAdmin');
+
+INSERT INTO `RolePermission` (`roleName`, `permissionName`)
+VALUES
+('ReferenceAdmin', 'reference:add:modify:entities'), ('ReferenceAdmin', 'reference:delete:entities');
+
+
