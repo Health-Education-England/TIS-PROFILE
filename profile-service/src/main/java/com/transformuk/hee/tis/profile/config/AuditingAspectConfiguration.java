508aa16f36a600bbc49e8569f8b40ecd2875d36f
srochani <sunil.rochani@hotmail.com>
2017-05-05 09:06:25 +0100

    [TISDEV-1889]: Add audit events to our Profile service using aspects oriented programming (#10)

---
+++ profile-service/src/main/java/com/transformuk/hee/tis/profile/config/AuditingAspectConfiguration.java
@@ -0,0 +1,20 @@
+package com.transformuk.hee.tis.profile.config;
+
+import com.transformuk.hee.tis.profile.aop.auditing.AuditingAspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AuditingAspectConfiguration {
+
+	@Bean
+	@Autowired
+	public AuditingAspect auditingAspect(AuditEventRepository auditEventRepository)
+	{
+		return new AuditingAspect(auditEventRepository);
+	}
+}
