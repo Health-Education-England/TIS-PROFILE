33a807cd4e1ab47db2dcebd648957575d48631f2
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-05-17 16:54:50 +0100

    added new etl user and linked it to the ETL role

--- profile-service/src/main/resources/db/migration/V2.5__TISDEV-1917_Consolidated_ETL_permissions.sql
+++ profile-service/src/main/resources/db/migration/V2.5__TISDEV-1917_Consolidated_ETL_permissions.sql
@@ -1,3 +1,8 @@
+
+INSERT INTO HeeUser
+VALUES ('consolidated_etl',NULL,'',NULL,NULL,'fake_email@fake.com',1);
+
 INSERT INTO `RolePermission` (`roleName`, `permissionName`)
-VALUES
-('ETL', 'reference:add:modify:entities');
+VALUES ('ETL', 'reference:add:modify:entities');
+
+INSERT INTO UserRole VALUES ('consolidated_etl','ETL');
