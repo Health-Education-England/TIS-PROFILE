903508c937ce5d0707deb0998abbde9c259a7cb6
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-06-19 15:56:40 +0100

    reapply the changes to reference config

--- profile-service/src/main/java/com/transformuk/hee/tis/profile/config/ReferenceClientConfiguration.java
+++ profile-service/src/main/java/com/transformuk/hee/tis/profile/config/ReferenceClientConfiguration.java
@@ -1,15 +1,15 @@
 package com.transformuk.hee.tis.profile.config;
 
 import com.transformuk.hee.tis.reference.client.config.ReferenceClientConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Import(ReferenceClientConfig.class)
-public class ReferenceClientConfiguration{
+public class ReferenceClientConfiguration extends ReferenceClientConfig{
 
-//	@Bean
-//	public RestTemplate referenceRestTemplate() {
-//		return super.prodBrowserInitiatedReferenceRestTemplate();
-//	}
+	@Bean
+	public RestTemplate referenceRestTemplate() {
+		return super.prodBrowserInitiatedReferenceRestTemplate();
+	}
 }
