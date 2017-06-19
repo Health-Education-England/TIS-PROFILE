29945c75286040a8970c381b98537daf3654db97
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-06-19 14:09:01 +0100

    [WIP] Fix shared configs (#16)
    * added config to use a reference rest template that extracts auth from the spring context, updated the dependencies
    
    * updated keycloak dependency
    
    * updated keycloak client dependency, removed configuration import as it should be up to the user of the client to do so
    
    * reverted major version change in pom, changed to patch
    
    * reverted major version change in pom, changed to patch

---
+++ profile-service/src/main/java/com/transformuk/hee/tis/profile/config/ReferenceClientConfiguration.java
@@ -0,0 +1,16 @@
+package com.transformuk.hee.tis.profile.config;
+
+import com.transformuk.hee.tis.reference.client.config.ReferenceClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ReferenceClientConfiguration extends ReferenceClientConfig {
+
+	@Bean
+	public RestTemplate referenceRestTemplate() {
+		return super.prodBrowserInitiatedReferenceRestTemplate();
+	}
+}
