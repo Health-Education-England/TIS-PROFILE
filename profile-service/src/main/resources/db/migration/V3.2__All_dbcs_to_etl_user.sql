f3c6abaf4712b6db208d2ba7278bceea6e14008d
alexdobre <alx.dobre@gmail.com>
2017-06-28 10:56:58 +0100

    - all designated bodies to consolidated ETL user

---
+++ profile-service/src/main/resources/db/migration/V3.2__All_dbcs_to_etl_user.sql
@@ -0,0 +1,14 @@
+INSERT INTO `UserDesignatedBody` (`userName`, `designatedBodyCode`)
+VALUES
+  ('consolidated_etl', '1-AIIDWT'),
+  ('consolidated_etl', '1-AIIDR8'),
+  ('consolidated_etl', '1-AIIDSA'),
+  ('consolidated_etl', '1-AIIDH1'),
+  ('consolidated_etl', '1-AIIDWA'),
+  ('consolidated_etl', '1-AIIDVS'),
+  ('consolidated_etl', '1-AIIDWI'),
+  ('consolidated_etl', '1-AIIDSI'),
+  ('consolidated_etl', '1-AIIDMY'),
+  ('consolidated_etl', '1-AIIDQQ'),
+  ('consolidated_etl', '1-AIIDNQ'),
+  ('consolidated_etl', '1-AIIDHJ');
