a7a739a245de9323852c032267722fa11afec61d
srochani <sunil.rochani@hotmail.com>
2017-07-12 15:41:33 +0100

    [TISDEV-2153] View all specialties server side (#22)

---
+++ profile-service/src/main/resources/db/migration/V3.6__Specialty_SpecialtyGroup_to_ETL.sql
@@ -0,0 +1,18 @@
+INSERT INTO `Permission` (`name`)
+VALUES
+('specialty:add:modify','PCS','Can create and modify specialty'),
+('specialty:view','PCS','Can view specialty'),
+('specialty:bulk:add:modify','INTERNAL','Used by the ETL to upsert intrepid data in bulk'),
+('specialty-group:add:modify','PCS','Can create and modify specialtyGroup'),
+('specialty-group:view','PCS','Can view specialtyGroup'),
+('specialty-group:bulk:add:modify','INTERNAL','Used by the ETL to upsert intrepid data in bulk');
+
+
+INSERT INTO `RolePermission` (`roleName`, `permissionName`)
+VALUES
+('ETL', 'specialty:add:modify'),
+('ETL', 'specialty:view'),
+('ETL', 'specialty:bulk:add:modify'),
+('ETL', 'specialty-group:add:modify'),
+('ETL', 'specialty-group:view'),
+('ETL', 'specialty-group:bulk:add:modify');
