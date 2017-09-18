08eb1dee26b5c3bd1e9ad8f6eca7de413cd4e7ec
srochani <sunil.rochani@hotmail.com>
2017-09-18 15:24:17 +0100

    People view permission

--- profile-api/src/main/java/com/transformuk/hee/tis/profile/dto/PermissionType.java
+++ profile-api/src/main/java/com/transformuk/hee/tis/profile/dto/PermissionType.java
@@ -4,5 +4,5 @@ package com.transformuk.hee.tis.profile.dto;
  * Holds all the permission types we can have
  */
 public enum PermissionType {
-  INTERNAL, CONCERN, CONNECTION, NOTIFICATION, PROFILE, PCS, PP, REVALIDATION, REFERENCE;
+  INTERNAL, CONCERN, CONNECTION, NOTIFICATION, PROFILE, PCS, PP, REVALIDATION, REFERENCE, PERSON;
 }
