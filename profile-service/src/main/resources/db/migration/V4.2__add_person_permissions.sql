d560e6256aeabca98fc68b3f283fa1ae36504ed4
srochani <sunil.rochani@hotmail.com>
2017-09-18 14:49:43 +0100

    People view permission (#28)
    * People view permission
    
    * People view permission

---
+++ profile-service/src/main/resources/db/migration/V4.2__add_person_permissions.sql
@@ -0,0 +1,15 @@
+INSERT INTO Permission (name, type, description,principal,resource,actions,effect) VALUES ('person:view', 'Person', 'Can view people','tis:profile::user:','tis:people::person:','View','Allow');
+INSERT INTO Permission (name, type, description,principal,resource,actions,effect) VALUES ('person:add:modify', 'Person', 'Can create and modify people','tis:profile::user:','tis:people::person:','Create,Update','Allow');
+INSERT INTO Permission (name, type, description,principal,resource,actions,effect) VALUES ('person:bulk:add:modify', 'Person', 'Used by the ETL to upsert intrepid data in bulk','tis:profile::user:consolidated_etl','tis:people::person:','Create,Update','Allow');
+
+INSERT INTO `RolePermission` (`roleName`, `permissionName`)
+VALUES
+('ETL', 'person:add:modify'),
+('ETL', 'person:bulk:add:modify'),
+('ETL', 'person:view');
+
+INSERT INTO `PermissionRequires` (`permission`, `requires`, `reason`)
+VALUES
+	('person:bulk:add:modify', 'person:view', 'User must be able to view people in order to change them.'),
+	('person:bulk:add:modify', 'person:add:modify', 'User must be able to modify one people in order to bulk modify them.'),
+	('person:add:modify', 'person:view', 'User must be able to view people in order to change them.');
