da98ed55527550af7eb9e936edc1aa1227c5b629
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-11-14 14:15:23 +0000

    change in permission names

--- profile-service/src/main/resources/db/migration/V4.4__Add_Assessment_permissions.sql
+++ profile-service/src/main/resources/db/migration/V4.4__Add_Assessment_permissions.sql
@@ -2,15 +2,15 @@ SET autocommit = 0;
 
 INSERT INTO `Permission` (`name`, `type`, `description`, `principal`, `resource`, `actions`, `effect`)
 VALUES
-	('assessment:add:modify', 'ASSESSMENT', 'can add or modify assessments', 'tis:profile::user:', 'tis:assessment:entity:', 'Create,Update', 'Allow'),
+	('assessment:add:modify:entities', 'ASSESSMENT', 'can add or modify assessments', 'tis:profile::user:', 'tis:assessment:entity:', 'Create,Update', 'Allow'),
 	('assessment:delete:entities', 'ASSESSMENT', 'can delete assessments', 'tis:profile::user:', 'tis:assessment:entity:', 'Delete', 'Allow'),
-	('assessment:view', 'ASSESSMENT', 'can view assessments', 'tis:profile::user:', 'tis:assessment:entity:', 'View', 'Allow');
+	('assessment:view:entities', 'ASSESSMENT', 'can view assessments', 'tis:profile::user:', 'tis:assessment:entity:', 'View', 'Allow');
 
 INSERT INTO `Permission` (`name`, `type`, `description`, `principal`, `resource`, `actions`, `effect`)
 VALUES
-	('outcomes:add:modify', 'ASSESSMENT', 'can add or modify outcomes', 'tis:profile::user:', 'tis:outcomes:entity:', 'Create,Update', 'Allow'),
+	('outcomes:add:modify:entities', 'ASSESSMENT', 'can add or modify outcomes', 'tis:profile::user:', 'tis:outcomes:entity:', 'Create,Update', 'Allow'),
 	('outcomes:delete:entities', 'ASSESSMENT', 'can delete outcomes', 'tis:profile::user:', 'tis:outcomes:entity:', 'Delete', 'Allow'),
-	('outcomes:view', 'ASSESSMENT', 'can view outcomes', 'tis:profile::user:', 'tis:outcomes:entity:', 'View', 'Allow');
+	('outcomes:view:entities', 'ASSESSMENT', 'can view outcomes', 'tis:profile::user:', 'tis:outcomes:entity:', 'View', 'Allow');
 
 INSERT INTO `Role` (`name`)
 VALUES
@@ -18,21 +18,21 @@ VALUES
 
 INSERT INTO `RolePermission` (`roleName`, `permissionName`)
 VALUES
-	('AssessmentsAdmin', 'assessment:add:modify'),
+	('AssessmentsAdmin', 'assessment:add:modify:entities'),
 	('AssessmentsAdmin', 'assessment:delete:entities'),
-	('AssessmentsAdmin', 'assessment:view'),
-	('AssessmentsAdmin', 'outcomes:add:modify'),
+	('AssessmentsAdmin', 'assessment:view:entities'),
+	('AssessmentsAdmin', 'outcomes:add:modify:entities'),
 	('AssessmentsAdmin', 'outcomes:delete:entities'),
-	('AssessmentsAdmin', 'outcomes:view');
+	('AssessmentsAdmin', 'outcomes:view:entities');
 
 INSERT INTO `RolePermission` (`roleName`, `permissionName`)
 VALUES
-	('ETL', 'assessment:add:modify'),
+	('ETL', 'assessment:add:modify:entities'),
 	('ETL', 'assessment:delete:entities'),
-	('ETL', 'assessment:view'),
-	('ETL', 'outcomes:add:modify'),
+	('ETL', 'assessment:view:entities'),
+	('ETL', 'outcomes:add:modify:entities'),
 	('ETL', 'outcomes:delete:entities'),
-	('ETL', 'outcomes:view');
+	('ETL', 'outcomes:view:entities');
 
 commit;
 
