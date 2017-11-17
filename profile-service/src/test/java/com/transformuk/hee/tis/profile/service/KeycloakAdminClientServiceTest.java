bf4646e72f80b3b2e0bf7660b3687c2f1560d7f2
paulhoang-hee <paul.hoang@hee.nhs.uk>
2017-11-17 14:23:40 +0000

    more tests

--- profile-service/src/test/java/com/transformuk/hee/tis/profile/service/KeycloakAdminClientServiceTest.java
+++ profile-service/src/test/java/com/transformuk/hee/tis/profile/service/KeycloakAdminClientServiceTest.java
@@ -1,8 +1,10 @@
 package com.transformuk.hee.tis.profile.service;
 
+import com.google.common.collect.Maps;
 import com.transform.hee.tis.keycloak.KeycloakAdminClient;
 import com.transform.hee.tis.keycloak.User;
 import com.transformuk.hee.tis.profile.domain.HeeUser;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,12 +15,21 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import javax.persistence.EntityManager;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static com.transformuk.hee.tis.profile.web.rest.TestUtil.createEntityHeeUser;
+import static junit.framework.TestCase.assertSame;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class KeycloakAdminClientServiceTest {
@@ -80,6 +91,18 @@ public class KeycloakAdminClientServiceTest {
     assertThat(userArg.getValue().getSurname()).isEqualTo(heeUser.getLastName());
     assertThat(userArg.getValue().getUsername()).isEqualTo(heeUser.getName());
 
+  }
+
+  @Test
+  public void getUserAttributesShouldReturnTheAttributesOnAUser() {
+    Map<String, List<String>> attributes = Maps.newHashMap();
+    when(keycloakAdminClient.getAttributesForUser(anyString(), eq(heeUser.getName()))).thenReturn(attributes);
+
+    Map<String, List<String>> result = keyclockAdminClientService.getUserAttributes(heeUser);
+
+    verify(keycloakAdminClient).getAttributesForUser("lin", "AAAAAAAAAA");
+
+    assertSame(attributes, result);
   }
 
 }
