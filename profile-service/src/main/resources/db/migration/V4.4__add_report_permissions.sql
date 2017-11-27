852205ab3cf86efc902f6d63c6f1a38d089a8f1f
Jagadeesh Kumar <jagadeesh.kumar@transformuk.com>
2017-11-27 15:41:40 +0000

    TISDEV-3212: Added a new permission to support revalidations report.

---
+++ profile-service/src/main/resources/db/migration/V4.4__add_report_permissions.sql
@@ -0,0 +1,1 @@
+INSERT INTO Permission (name, type, description,principal,resource,actions,effect) VALUES ('revalidation:see:dbc:report', 'REVALIDATION', 'Can view revalidation report','tis:revalidation::user:','tis:revalidation::trainee:','View','Allow');
