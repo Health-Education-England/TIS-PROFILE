b13c7f04cd782fa9f6fc79304736ae3f9fe97be7
Jagadeesh Kumar <jagadeesh.kumar@transformuk.com>
2017-11-27 16:08:57 +0000

    TISDEV-3212: Bumped pom version and reformatted sql.

--- profile-service/src/main/resources/db/migration/V4.4__add_report_permissions.sql
+++ profile-service/src/main/resources/db/migration/V4.4__add_report_permissions.sql
@@ -1,1 +1,4 @@
-INSERT INTO Permission (name, type, description,principal,resource,actions,effect) VALUES ('revalidation:see:dbc:report', 'REVALIDATION', 'Can view revalidation report','tis:revalidation::user:','tis:revalidation::trainee:','View','Allow');
+INSERT INTO `Permission`
+  (`name`, `type`, `description`, `principal`, `resource`, `actions`, `effect`)
+VALUES
+  ('revalidation:see:dbc:report', 'REVALIDATION', 'Can view revalidation report','tis:revalidation::user:','tis:revalidation::trainee:','View','Allow');
