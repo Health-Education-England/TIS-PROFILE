f6d6454fc727548c9b2fbaedcd3ed2210eed2255
Suneet Kamath <suneet.kamath@hee.nhs.uk>
2018-02-28 14:23:08 +0000

    TISDEV-2579 patch for broken build; reverting foreign keys
    Author : @sfkamath

--- profile-service/src/main/resources/db/migration/V4.7__bulk_upload_person.sql
+++ profile-service/src/main/resources/db/migration/V4.7__bulk_upload_person.sql
@@ -1,5 +1,3 @@
-ALTER TABLE `Permission` DROP PRIMARY KEY, ADD PRIMARY KEY(`name`, `type`);
-
 INSERT INTO `Role` (`name`) VALUES ('BulkUploadAdmin');
 
 INSERT INTO `RolePermission` (`roleName`, `permissionName`)
@@ -7,11 +5,4 @@ VALUES
 ('BulkUploadAdmin', 'person:add:modify'),
 ('BulkUploadAdmin', 'curriculum:view'),
 ('BulkUploadAdmin', 'programme:view');
-
-INSERT INTO `Permission` (`name`, `type`, `description`, `principal`, `resource`, `actions`, `effect`)
-VALUES
-('curriculum:view', 'BULK_UPLOAD', 'Can view curricula', 'tis:profile::user:bulk_upload', 'tis:curricula:-:curriculum:', 'View', 'Allow'),
-('programme:view', 'BULK_UPLOAD', 'Can view programme', 'tis:profile::user:bulk_upload', 'tis:programmes::programme:', 'View', 'Allow'),
-('person:add:modify', 'BULK_UPLOAD', 'Can create and modify people', 'tis:profile::user:bulk_upload', 'tis:people::person:', 'Create,Update', 'Allow');
-
 
