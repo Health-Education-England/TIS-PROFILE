047d8e50aabd12339a1a2d37f83e112d112b0347
simonmeredith <simonmeredith1@gmail.com>
2018-03-08 12:54:13 +0000

    Revert back to Dec 11 version (profile service 2.6.11 - but renamed as 2.6.16) (#44)
    * reverting to Dec 11 changes
    
    * bumped poms

--- profile-service/src/main/resources/db/migration/V4.7__bulk_upload_person.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `Role` (`name`) VALUES ('BulkUploadAdmin');
-
-INSERT INTO `RolePermission` (`roleName`, `permissionName`)
-VALUES
-('BulkUploadAdmin', 'person:add:modify'),
-('BulkUploadAdmin', 'curriculum:view'),
-('BulkUploadAdmin', 'programme:view');
-
