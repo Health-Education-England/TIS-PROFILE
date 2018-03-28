6882c343c3a7cea33f8e775c3d3fc8049857e789
simonmeredith <simonmeredith1@gmail.com>
2018-03-28 15:33:48 +0100

    Add delete perms (#52)
    * added delete perms
    
    * added delete perms

---
+++ profile-service/src/main/resources/db/migration/V5.7__add_delete_perms.sql
@@ -0,0 +1,25 @@
+INSERT INTO `RolePermission` (`roleName`,`permissionName`)
+VALUES
+  ('Hee Admin','assessment:delete:entities'),
+  ('Hee Admin','tcs:delete:entities')
+ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
+
+INSERT INTO `RolePermission` (`roleName`,`permissionName`)
+VALUES
+  ('Tis Admin','assessment:delete:entities'),
+  ('Tis Admin','reference:delete:entities'),
+  ('Tis Admin','tcs:delete:entities'),
+  ('Tis Admin','profile:delete:entities')
+ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
+
+INSERT INTO `RolePermission` (`roleName`,`permissionName`)
+VALUES
+  ('HEE Admin Sensitive','assessment:delete:entities'),
+  ('HEE Admin Sensitive','tcs:delete:entities')
+ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
+
+INSERT INTO `RolePermission` (`roleName`,`permissionName`)
+VALUES
+  ('HEE Admin Revalidation','assessment:delete:entities'),
+  ('HEE Admin Revalidation','tcs:delete:entities')
+ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
