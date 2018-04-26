6a151f0a04c5e493c0c48584fc72cd0759762ec5
simonmeredith <simonmeredith1@gmail.com>
2018-04-26 15:33:25 +0100

    Revert "TISDEV-3119 permissions to add/modify placements and view posts"
    This reverts commit 730bba7282c47a65193128e3b2c3c9f9241aae54.

--- profile-service/src/main/resources/db/migration/V5.9__bulk_upload_placement.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `RolePermission` (`roleName`, `permissionName`)
-VALUES
-('BulkUploadAdmin', 'post:view'),
-('BulkUploadAdmin', 'tcs:add:modify:entities'),
-('BulkUploadAdmin', 'tcs:view:entities');
-
