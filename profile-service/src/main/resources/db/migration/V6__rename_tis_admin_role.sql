30848e151d54a9aadb79fc741660a382d9267f02
simonmeredith <simonmeredith1@gmail.com>
2018-04-27 11:01:17 +0100

    formatted flyway scripts

--- profile-service/src/main/resources/db/migration/V6__rename_tis_admin_role.sql
+++ profile-service/src/main/resources/db/migration/V6__rename_tis_admin_role.sql
@@ -2,19 +2,25 @@ INSERT INTO `Role` (`name`)
 VALUES
 ('HEE TIS Admin')
 ON DUPLICATE KEY UPDATE `name` = `name`;
+
 UPDATE `RolePermission`
 SET `roleName` = 'HEE TIS Admin'
 WHERE `roleName` = 'TIS Admin';
+
 UPDATE `UserRole`
 SET `roleName` = 'HEE TIS Admin'
 WHERE `roleName` = 'TIS Admin';
+
 UPDATE `UserRole`
 SET `roleName` = 'HEE TIS Admin'
 WHERE `roleName` = 'TisAdmin';
+
 INSERT INTO `RolePermission`
 VALUES
 ('HEE TIS Admin','notification:change:add:notification')
 ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
+
 DELETE FROM `RolePermission` WHERE `roleName` = 'TisAdmin';
+
 DELETE FROM `Role` where `name` = 'TIS Admin';
 DELETE FROM `Role` where `name` = 'TisAdmin';
