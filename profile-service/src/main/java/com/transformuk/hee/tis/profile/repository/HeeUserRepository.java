984088321d31f70c1e17dfff074d7e5e97208896
paulhoang-hee <paul.hoang@hee.nhs.uk>
2018-09-11 13:44:09 +0100

    added test that tests lazy fetching between HeeUser and UserTrust from the HeeUser side

--- profile-service/src/main/java/com/transformuk/hee/tis/profile/repository/HeeUserRepository.java
+++ profile-service/src/main/java/com/transformuk/hee/tis/profile/repository/HeeUserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import java.util.Optional;
  * Spring Data JPA repository for the HeeUser entity.
  */
 @SuppressWarnings("unused")
+@Repository
 public interface HeeUserRepository extends JpaRepository<HeeUser, String>, JpaSpecificationExecutor<HeeUser> {
 
   /**
--- profile-service/src/main/java/com/transformuk/hee/tis/profile/service/UserService.java
+++ profile-service/src/main/java/com/transformuk/hee/tis/profile/service/UserService.java
@@ -1,7 +1,14 @@
 package com.transformuk.hee.tis.profile.service;
 
+import com.transformuk.hee.tis.profile.service.dto.HeeUserDTO;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+  public List<HeeUserDTO> findAllUsersWithTrust(Pageable pageable) {
+    return null;
+  }
 }
