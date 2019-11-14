3c3561d7dccdd766a47bdf3682fe9204a40193ec
Yafang Deng <hihilary@outlook.com>
2019-11-14 10:27:19 +0000

    Add placement approve perm.

---
+++ profile-service/src/main/resources/db/migration/V9.6__add_placement_approve_perm_for_HEEadminRole.sql
@@ -0,0 +1,16 @@
+INSERT INTO `Permission`(`name`, `type`, `description`, `principal`, `resource`, `actions`, `effect`)
+VALUES
+	  ('placement:approve',
+	  'PCS',
+	  'can approve placement',
+	  'tis:profile::user:',
+	  'tis:placements::placement:',
+	  'Update',
+	  'Allow')
+ON DUPLICATE KEY UPDATE `name` = `name`;
+
+INSERT INTO `RolePermission` (`roleName`, `permissionName`)
+VALUES
+      ('HEE Admin','placement:approve'),
+      ('HEE TIS Admin','placement:approve')
+ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
