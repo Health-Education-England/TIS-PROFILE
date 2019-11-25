9b83bbb91e0de62553c99f5522f6ea5895b7dde5
Yafang Deng <33690649+hihilary@users.noreply.github.com>
2019-11-25 10:01:25 +0100

    TISNEW-3492 add permission for draft placement approval (#107)
    * Add placement approve perm.
    
    * Add placement approve perm to BulkUploadAdmin
    
    * fix the type of placement approval permission
    
    * increase the version number

---
+++ profile-service/src/main/resources/db/migration/V9.6__add_placement_approve_perm_for_HEEadminRole.sql
@@ -0,0 +1,17 @@
+INSERT INTO `Permission`(`name`, `type`, `description`, `principal`, `resource`, `actions`, `effect`)
+VALUES
+	  ('placement:approve',
+	  'PP',
+	  'can approve placement',
+	  'tis:profile::user:',
+	  'tis:placements::placement:',
+	  'Update',
+	  'Allow')
+ON DUPLICATE KEY UPDATE `name` = `name`;
+
+INSERT INTO `RolePermission` (`roleName`, `permissionName`)
+VALUES
+      ('HEE Admin', 'placement:approve'),
+      ('HEE TIS Admin', 'placement:approve'),
+      ('BulkUploadAdmin', 'placement:approve')
+ON DUPLICATE KEY UPDATE `roleName` = `roleName`;
