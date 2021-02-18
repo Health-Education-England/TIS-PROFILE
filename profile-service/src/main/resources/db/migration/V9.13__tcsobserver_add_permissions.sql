9fbd3d7166b9a7bf8879236648d8d7f3ab75c003
JosephKelly <joseph.kelly@hee.nhs.uk>
2021-02-18 15:55:18 +0000

    feat: extend `TcsObserver`s read permission
    The sync_service is the only user in HEE that doesn't have all permissions.
    
    TIS21-1032: Trainee Sync Placements

---
+++ profile-service/src/main/resources/db/migration/V9.13__tcsobserver_add_permissions.sql
@@ -0,0 +1,4 @@
+INSERT INTO RolePermission VALUES ('TcsObserver','post:view');
+INSERT INTO RolePermission VALUES ('TcsObserver','curriculum:view');
+INSERT INTO RolePermission VALUES ('TcsObserver','programme:view');
+INSERT INTO RolePermission VALUES ('TcsObserver','specialty:view');
