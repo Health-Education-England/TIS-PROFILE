7c3f0f577f7ed0bcf62e5c0f8dc0ee0f851b54e9
Yafang Deng <hihilary@outlook.com>
2023-03-27 23:09:12 +0100

    chore:update user dbc
    TIS21-4236

---
+++ profile-service/src/main/resources/db/migration/V9.20__update_user_designated_body.sql
@@ -0,0 +1,16 @@
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDQQ';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDSA';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDWT';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDSI';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDNQ';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDWI';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDH1';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDHJ';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDMQ';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDMY';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDR8';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDVS';
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-AIIDWA';
+
+UPDATE `UserDesignatedBody` set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-5PBFHF';
+UPDATE `UserDesignatedBod`y set `designatedBodyCode` = '1-***' where `designatedBodyCode` = '1-2SXJST';
