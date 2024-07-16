5b2e482d62a4ccd6058dd8429477d7d1a6f26beb
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2024-07-16 11:39:57 +0100

    fix: create user if does not exist (eg. preprod)

--- profile-service/src/main/resources/db/migration/common/V9.26__add_sync_profile_observe_permission.sql
+++ profile-service/src/main/resources/db/migration/common/V9.26__add_sync_profile_observe_permission.sql
@@ -1,3 +1,8 @@
+INSERT INTO `HeeUser` (`name`, `firstName`, `lastName`, `emailAddress`, `active`)
+VALUES
+    ('sync_service', 'TIS', 'Sync-Service', 'sync_service@tis.nhs.uk', 1)
+ON DUPLICATE KEY UPDATE `name` = `name`;
+
 INSERT INTO `UserRole` (`userName`, `roleName`)
 VALUES
     ('sync_service', 'ProfileObserver')
