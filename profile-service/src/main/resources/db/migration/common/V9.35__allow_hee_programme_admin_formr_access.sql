9a7380a6fa1371b09d97583204c0144149bd33fd
jayanta2018 <jayanta.saha@hee.nhs.uk>
2026-09-10 17:31:59 +0100

    refact: allow hee programme admin formr access

---
+++ profile-service/src/main/resources/db/migration/common/V9.35__allow_hee_programme_admin_formr_access.sql
@@ -0,0 +1,5 @@
+INSERT INTO `RolePermission`(`roleName`, `permissionName`)
+VALUES ('HEE Programme Admin', 'trainee-formr:delete'),
+       ('HEE Programme Admin', 'trainee-formr:view')
+        ON DUPLICATE KEY
+UPDATE `roleName` = `roleName`,`permissionName` = `permissionName`;
