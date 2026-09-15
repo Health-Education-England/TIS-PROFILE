ba80058ca40f4d962dab34351acb9ee61521351b
jayanta2018 <jayanta.saha@hee.nhs.uk>
2026-09-15 15:36:46 +0100

    fix: remove hee programme admin from delete formr access

--- profile-service/src/main/resources/db/migration/common/V9.35__allow_hee_programme_admin_formr_access.sql
+++ profile-service/src/main/resources/db/migration/common/V9.35__allow_hee_programme_admin_formr_access.sql
@@ -1,5 +1,4 @@
 INSERT INTO `RolePermission`(`roleName`, `permissionName`)
-VALUES ('HEE Programme Admin', 'trainee-formr:delete'),
-       ('HEE Programme Admin', 'trainee-formr:view')
+VALUES ('HEE Programme Admin', 'trainee-formr:view')
         ON DUPLICATE KEY
 UPDATE `roleName` = `roleName`,`permissionName` = `permissionName`;
